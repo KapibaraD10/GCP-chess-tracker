4a0bc8ed32fd84297ddb1877631de89597435d3d
Kacper Karabasz <karambemail@gmail.com>
2026-01-03 20:59:56 +0100

    Use cloudbuild.yaml for build config

--- terraform/cloud_build.tf
+++ terraform/cloud_build.tf
@@ -15,35 +15,12 @@ resource "google_cloudbuild_trigger" "build_main" {
     }
   }
 
-  build {
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [
-        "build",
-        "-t", "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.service_name}:$COMMIT_SHA",
-        "-f", "src/Dockerfile",
-        "./src"
-      ]
-    }
-    step {
-      name = "gcr.io/cloud-builders/docker"
-      args = [
-        "push",
-        "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.service_name}:$COMMIT_SHA"
-      ]
-    }
-    step {
-      name = "gcr.io/google.com/cloudsdktool/cloud-sdk"
-      entrypoint = "gcloud"
-      args = [
-        "run", "deploy", var.service_name,
-        "--image", "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}/${var.service_name}:$COMMIT_SHA",
-        "--region", var.region,
-        "--platform", "managed",
-        "--port", "8080",
-        "--allow-unauthenticated"
-      ]
-    }
+  filename = "cloudbuild.yaml"
+
+  substitutions = {
+    _REGION       = var.region
+    _REPO_NAME    = var.repo_name
+    _SERVICE_NAME = var.service_name
   }
 
   depends_on = [google_project_service.apis, google_artifact_registry_repository.repo]
